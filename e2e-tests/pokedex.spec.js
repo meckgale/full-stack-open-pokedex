055e613e8f6c0871f9a21c70c339b5fff9459a65
meckgalen <meckgalen@gmail.com>
2026-02-03 17:02:22 +0300

    add page navigation e2e test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -10,4 +10,10 @@ describe('Pokedex', () => {
       ),
     ).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
